724bf82f9eea2fe2529c1cbfeb29018c194393d5
SimonStorlSchulke <94753530+FantasticMrsFox@users.noreply.github.com>
2021-12-05 16:46:04 +0100

    stuff + Sheep AI reset

--- ShepherdsPath/src/Actors/Sheep/flock.gd
+++ ShepherdsPath/src/Actors/Sheep/flock.gd
@@ -19,7 +19,7 @@ func init_flock():
 	for _i in range(sheep_number):
 		var c_sheep: Sheep = Sheep.instance()
 		#var lPos: Vector3 = global_transform.origin
-		c_sheep.randomize_behaviour()
+		#c_sheep.get_node("StateMachine").randomize_behaviour()
 		c_sheep.translation += Vector3(rand_range(spawn_radius, -spawn_radius), 0, rand_range(spawn_radius, -spawn_radius))
 		##c_sheep.global_transform.origin = lPos
 		add_child(c_sheep)
